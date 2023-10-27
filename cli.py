e5167a498f939662b5e5c20f414bfd7534a5b574
diodioko <jiayi.xia10@gmail.com>
2023-10-27 13:59:43 -0700

    modified:   cli.py

--- cli.py
+++ cli.py
@@ -2,23 +2,12 @@
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
 
-from logic import make_empty_board
+from logic import make_empty_board, get_winner, other_player
 
-# Reminder to check all the tests
-
-if __name__ == '__main__':
-    board = make_empty_board()
-    winner = None
-    while winner == None:
-        print("TODO: take a turn!")
-        # TODO: Show the board to the user.
-        # TODO: Input a move from the player.
-        # TODO: Update the board.
-        # TODO: Update who's turn it is.
-        winner = 'X'  # FIXME
- 
-
-from logic import make_empty_board, check_winner, print_board
+def print_board(board):
+    for row in board:
+        print(" | ".join([cell or ' ' for cell in row]))
+        print("---------")
 
 if __name__ == '__main__':
     board = make_empty_board()
@@ -26,35 +15,31 @@ if __name__ == '__main__':
     current_player = 'X'
 
     while winner is None:
+        print("Current board:")
         print_board(board)
-        print(f"Player {current_player}'s turn")
-
+        
+        print(f"It's {current_player}'s turn.")
+        
         # Input a move from the player
         while True:
             try:
-                row = int(input("Enter row (0, 1, or 2): "))
-                col = int(input("Enter column (0, 1, or 2): "))
-
-                if 0 <= row < 3 and 0 <= col < 3 and board[row][col] == ' ':
+                row = int(input("Enter the row (0, 1, or 2): "))
+                col = int(input("Enter the column (0, 1, or 2): "))
+                if board[row][col] is None:
                     board[row][col] = current_player
                     break
                 else:
-                    print("Invalid move. Try again.")
+                    print("That spot is already taken. Try again.")
             except (ValueError, IndexError):
-                print("Invalid input. Please enter row and column as integers.")
-
-        # Check if the current player has won
-        if check_winner(board, current_player):
-            print_board(board)
-            print(f"Player {current_player} wins!")
-            winner = current_player
-            break
-        # Check if the board is full (a draw)
-        elif all(cell != ' ' for row in board for cell in row):
-            print_board(board)
-            print("It's a draw!")
-            winner = 'Draw'
+                print("Invalid input. Try again.")
+        
+        # Check for a winner
+        winner = get_winner(board)
+        if winner:
+            print(f"{winner} wins!")
+        elif all(cell is not None for row in board for cell in row):
+            print("It's a tie!")
             break
 
         # Switch to the other player
-        current_player = 'O' if current_player == 'X' else 'X'
+        current_player = other_player(current_player)
