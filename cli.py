f40dec0df6d05e2b8933d2d8895b28ea213262bf
diodioko <148396856+diodioko@users.noreply.github.com>
2023-11-08 17:39:07 -0800

    Add files via upload

--- cli.py
+++ cli.py
@@ -1,98 +1,18 @@
 # This file contains the Command Line Interface (CLI) for
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
-import logging
-from logic import TicTacToe
-
-logging.basicConfig(
-    filename='logs/game_records.log',
-    level=logging.INFO
-)
-
-def print_board(board):
-    for row in board:
-        print(" | ".join([cell or ' ' for cell in row]))
-        print("---------")
-
-def play_game_single_player():
-    game = TicTacToe()
-    while game.winner is None:
-        print("Current board:")
-        print_board(game.board)
-        print(f"It's {game.current_player}'s turn.")
-
-        if game.current_player == 'X':
-            while True:
-                try:
-                    row = int(input("Enter the row (0, 1, or 2): "))
-                    col = int(input("Enter the column (0, 1, or 2): "))
-                    game.make_move(row, col)
-                    break
-                except (ValueError, IndexError):
-                    print("Invalid input. Try again.")
-        else:  # Bot's turn
-            # Add your bot logic here (e.g., random moves)
-            # For now, let's make random moves for the bot
-            import random
-            while True:
-                row = random.randint(0, 2)
-                col = random.randint(0, 2)
-                if game.board[row][col] is None:
-                    game.make_move(row, col)
-                    break
-
-        winner = game.winner
-        if winner == 'X' or winner == 'O':
-            print_board(game.board)
-            print(f"{winner} wins!")
-            logging.info(f'{winner} won')
-        elif winner == 'Tie':
-            print_board(game.board)
-            print("It's a tie!")
-            logging.info('Tie')
-
-def play_game_two_players():
-    game = TicTacToe()
-    while game.winner is None:
-        print("Current board:")
-        print_board(game.board)
-        print(f"It's {game.current_player}'s turn.")
-        while True:
-            try:
-                row = int(input("Enter the row (0, 1, or 2): "))
-                col = int(input("Enter the column (0, 1, or 2): "))
-                game.make_move(row, col)
-                break
-            except (ValueError, IndexError):
-                print("Invalid input. Try again.")
-
-        winner = game.winner
-        if winner == 'X' or winner == 'O':
-            print_board(game.board)
-            print(f"{winner} wins!")
-            logging.info(f'{winner} won')
-        elif winner == 'Tie':
-            print_board(game.board)
-            print("It's a tie!")
-            logging.info('Tie')
+ 
+from logic import Game, Human, Bot
 
 if __name__ == '__main__':
-    print("Welcome to Tic-Tac-Toe!")
-    print("Choose the game mode:")
-    print("1. Single Player (against a bot)")
-    print("2. Two Players")
-    
-    while True:
-        try:
-            choice = int(input("Enter 1 or 2: "))
-            if choice in (1, 2):
-                break
-            else:
-                print("Invalid choice. Try again.")
-        except ValueError:
-            print("Invalid input. Try again.")
+    game_mode = input("Enter game mode (1 for single player, 2 for two players): ")
 
-    if choice == 1:
-        play_game_single_player()
+    if game_mode == '1':
+        game = Game(Human(), Bot())
+    elif game_mode == '2':
+        game = Game(Human(), Human())
     else:
-        play_game_two_players()
+        print("Invalid game mode. Exiting.")
+        exit()
+
+    game.run()
--- logic.py
+++ logic.py
@@ -2,44 +2,98 @@
 # or output happens here. The logic in this file
 # should be unit-testable.
 
-class TicTacToe:
-    def __init__(self):
-        self.board = self.make_empty_board()
-        self.current_player = 'X'
-        self.winner = None
+import random
 
-    @staticmethod
-    def make_empty_board():
-        return [
+class Board:
+    def __init__(self):
+        self._board = [
             [None, None, None],
             [None, None, None],
             [None, None, None]
         ]
 
-    def make_move(self, row, col):
-        if self.board[row][col] is None:
-            self.board[row][col] = self.current_player
-            self.switch_player()
-            self.check_winner()
+    def make_move(self, row, col, player):
+        if self._board[row][col] is None:
+            self._board[row][col] = player
+            return True
+        return False
 
-    def switch_player(self):
-        if self.current_player == 'X':
-            self.current_player = 'O'
-        else:
-            self.current_player = 'X'
+    def get_board(self):
+        return self._board
 
-    def check_winner(self):
+class Game:
+    def __init__(self, playerX, playerO):
+        self._board = Board()
+        self._playerX = playerX
+        self._playerO = playerO
+        self._current_player = 'X'
+
+    def run(self):
+        while True:
+            print("Current board:")
+            self.print_board()
+
+            print(f"It's {self._current_player}'s turn.")
+
+            if self._current_player == 'X':
+                move = self._playerX.get_move(self._board.get_board())
+            else:
+                move = self._playerO.get_move(self._board.get_board())
+
+            row, col = move
+            if self._board.make_move(row, col, self._current_player):
+                winner = self.get_winner()
+                if winner:
+                    print("Current board:")
+                    self.print_board()
+                    print(f"{winner} wins!")
+                    break
+                elif all(cell is not None for row in self._board.get_board() for cell in row):
+                    print("Current board:")
+                    self.print_board()
+                    print("It's a tie!")
+                    break
+
+                self._current_player = 'O' if self._current_player == 'X' else 'X'
+
+    def print_board(self):
+        for row in self._board.get_board():
+            print(" | ".join([cell or ' ' for cell in row]))
+            print("---------")
+
+    def get_winner(self):
         for player in ['X', 'O']:
-            for row in self.board:
+            # Check rows
+            for row in self._board.get_board():
                 if all(cell == player for cell in row):
-                    self.winner = player
+                    return player
 
+            # Check columns
             for col in range(3):
-                if all(self.board[row][col] == player for row in range(3)):
-                    self.winner = player
+                if all(row[col] == player for row in self._board.get_board()):
+                    return player
 
-            if all(self.board[i][i] == player for i in range(3)) or all(self.board[i][2 - i] == player for i in range(3)):
-                self.winner = player
+            # Check diagonals
+            if all(self._board.get_board()[i][i] == player for i in range(3)) or all(
+                    self._board.get_board()[i][2 - i] == player for i in range(3)):
+                return player
 
-        if all(cell is not None for row in self.board for cell in row):
-            self.winner = 'Tie'
+        return None
+
+class Human:
+    def get_move(self, board):
+        while True:
+            try:
+                row = int(input("Enter the row (0, 1, or 2): "))
+                col = int(input("Enter the column (0, 1, or 2): "))
+                if board[row][col] is None:
+                    return row, col
+                else:
+                    print("That spot is already taken. Try again.")
+            except (ValueError, IndexError):
+                print("Invalid input. Try again.")
+
+class Bot:
+    def get_move(self, board):
+        available_moves = [(i, j) for i in range(3) for j in range(3) if board[i][j] is None]
+        return random.choice(available_moves)
